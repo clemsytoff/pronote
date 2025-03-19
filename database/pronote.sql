9ff1251844e5917a6ad308db447587a77d5dec7c
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2025-03-19 08:38:45 +0100

    Début sql

---
+++ database/pronote.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    classe VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    account_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    grade INT NOT NULL
+);
